a55948e2fd68d6de91edf6f0298165c09d50db7f
YaseenBCity <Yaseen.Barlas@city.ac.uk>
2024-12-31 12:37:53 +0000

    dotenv installation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
 
 app.use(express.json()); // Adding middleware
 app.use('/', routes);
@@ -11,4 +12,5 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3030;
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
