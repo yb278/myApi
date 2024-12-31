5c280ebf372639559725fee1872f554242325655
YaseenBCity <Yaseen.Barlas@city.ac.uk>
2024-12-31 08:26:20 +0000

    put command working

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json()); //Adding middleware 
 
-const courses = [
+let courses = [
     {id:1, name: 'course1'},
     {id:2, name: 'course2'},
     {id:3, name: 'course3'},
 ];
 
+//GET
 app.get('/', (req, res) =>{
     res.send('Hello World');
 });
-
+//GET
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
+//GET
+app.get('/api/courses/:id', (req,res) =>{
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('Course not found');
+    res.send(course);
+});
 
+//POST
 app.post('/api/courses' , (req, res)=>{
-    if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send('Invalid Course Type');
+    const result = validateCourse(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
         return;
     }
+
     const course = {
         id: courses.length + 1, 
         name: req.body.name
@@ -30,13 +42,30 @@ app.post('/api/courses' , (req, res)=>{
     res.send(course);
 });
 
-
-
-app.get('/api/courses/:id', (req,res) =>{
+//PUT
+app.put('/api/courses/:id' , (req, res)=>{
+    //Find course with id, validate id, then update
     let course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('Course not found');
+    if(!course) res.status(404).send('Course not found'); 
+
+    
+    const result = validateCourse(req.body);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    
+    course.name = req.body.name;
     res.send(course);
+
 });
+
+//Validation Function
+function validateCourse(course){
+    const schema = Joi.object({name: Joi.string().min(3).required()});
+    return schema.validate(course);
+};
+
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`Listening on port ${port}`));
